98130dd41f8fd7b967662096332b3efc77d393ff
andresuriza <urizandres@estudiantec.cr>
2025-09-16 22:45:35 -0600

    Version casi funcional de nombre histograma

--- server/ImgFunciones.c
+++ server/ImgFunciones.c
@@ -250,9 +250,146 @@ static int WriteFile(const char* path, int w, int h, int nchan, const uint8_t* d
     return ok;
 }
 
-void CalcHist(const char* in_path) {
-    const char* out_path = "../imagenes_out/Histograma/out.jpg";
 
+// Obtiene nombre y extension de imagen
+static const char* GetFileN(const char* path) 
+{
+    const char* slash = strrchr(path, '/');
+    const char* base = path;
+    
+    base = slash + 1;
+    
+    return base;
+}
+
+// Verifica directorio
+static void CheckDir(const char* path) 
+{
+    if (!path || !*path) 
+    return;
+    
+    char tmp[1024];
+    size_t n = strlen(path);
+    
+    if (n >= sizeof(tmp)) 
+    p_error("Path too long");
+    
+    strcpy(tmp, path);
+    
+    while (n > 0 && (tmp[n-1] == '/')) 
+    { 
+        tmp[n-1] = '\0'; n--; 
+    }
+    
+    if (n == 0) 
+    return;
+
+    char buf[1024] = {0};
+    size_t len = 0;
+    for (size_t i = 0; i < n; ++i) 
+    {
+        char c = tmp[i];
+        if (c == '/') 
+        {
+            if (len > 0) 
+            {
+                if (MKDIR(buf) != 0) { /* ignora errores */ }
+            }
+        } 
+        
+        else 
+        {
+            size_t bl = strlen(buf);
+            if (bl + 2 >= sizeof(buf)) p_error("Path muy largo");
+            buf[bl] = c; buf[bl+1] = '\0';
+        }
+        
+        if (c == '/') 
+        {
+            size_t bl = strlen(buf);
+            if (bl + 2 >= sizeof(buf)) p_error("Path muy largo");
+            buf[bl] = PATHSEP; buf[bl+1] = '\0';
+        }
+    }
+    
+    if (MKDIR(buf) != 0) { /* ignorar si existe */ }
+}
+
+// Lee imagen y clasifica en base a color predominante
+void Clasificar(const char* img_in) {
+    const char* img_out = Get_Dircolores();
+    
+    int w, h, ch;
+    
+    unsigned char* img = stbi_load(img_in, &w, &h, &ch, 0);
+    
+    if (!img) 
+    p_error("Error al cargar imagen");
+    
+    if (!(ch == 1 || ch == 3 || ch == 4)) 
+    {
+        stbi_image_free(img);
+        img = stbi_load(img_in, &w, &h, &ch, 3);
+        
+        if (!img) 
+        p_error("Cantidad de canales de imagen invalido");
+        
+        ch = 3;
+    }
+    
+    size_t pixeles = (size_t)w * (size_t)h;
+    
+    unsigned long long sumR = 0, sumV = 0, sumA = 0;
+    
+    if (ch == 1) 
+    {
+        for (size_t i = 0; i < pixeles; ++i) 
+        {
+            unsigned int val = img[i];
+            sumR += val; 
+            sumV += val; 
+            sumA += val;
+        }
+    } 
+    
+    else if (ch == 3 || ch == 4) 
+    {
+        for (size_t i = 0, p = 0; i < pixeles; ++i, p += ch) 
+        {
+            sumR += img[p + 0];
+            sumV += img[p + 1];
+            sumA += img[p + 2];
+        }
+    }
+    
+    const char* folder = "Rojo";
+    
+    if (sumV > sumR && sumV >= sumA) 
+    folder = "Verde";
+    
+    else if (sumA > sumR && sumA > sumV) 
+        folder = "Azul";
+
+        char folderOut[1024];
+        snprintf(folderOut, sizeof(folderOut), "%s%c%s", img_out, PATHSEP, folder);
+        CheckDir(img_out);
+        CheckDir(folderOut);
+        
+        const char* base = GetFileN(img_in);
+        
+        char out_path[1200];
+        snprintf(out_path, sizeof(out_path), "%s%c%s", folderOut, PATHSEP, base);
+    
+        int ok = stbi_write_png(out_path, w, h, ch, img, w * ch);
+        stbi_image_free(img);
+        
+        if (!ok) 
+        p_error("Error al escribir PNG.");
+    }
+
+void CalcHist(const char* in_path) {
+    //const char* out_path = "../imagenes_out/Histograma/out.jpg";
+    const char* out_path = Get_Dirhisto();
     // Cambiar a 1 si es YCbCr, (solo luminancia)
     int mode_rgb = 0; 
 
@@ -281,142 +418,4 @@ void CalcHist(const char* in_path) {
         stbi_image_free(img);
         p_error("Error al escribir imagen");
     }
-
-    stbi_image_free(img);
-}
-
-// Obtiene nombre y extension de imagen
-static const char* GetFileN(const char* path) 
-{
-    const char* slash = strrchr(path, '/');
-    const char* base = path;
-
-    base = slash + 1;
-
-    return base;
-}
-
-// Verifica directorio
-static void CheckDir(const char* path) 
-{
-    if (!path || !*path) 
-        return;
-
-    char tmp[1024];
-    size_t n = strlen(path);
-
-    if (n >= sizeof(tmp)) 
-        p_error("Path too long");
-
-    strcpy(tmp, path);
-
-    while (n > 0 && (tmp[n-1] == '/')) 
-    { 
-        tmp[n-1] = '\0'; n--; 
-    }
-
-    if (n == 0) 
-        return;
-
-    char buf[1024] = {0};
-    size_t len = 0;
-    for (size_t i = 0; i < n; ++i) 
-    {
-        char c = tmp[i];
-        if (c == '/') 
-        {
-            if (len > 0) 
-            {
-                if (MKDIR(buf) != 0) { /* ignora errores */ }
-            }
-        } 
-        
-        else 
-        {
-            size_t bl = strlen(buf);
-            if (bl + 2 >= sizeof(buf)) p_error("Path muy largo");
-            buf[bl] = c; buf[bl+1] = '\0';
-        }
-
-        if (c == '/') 
-        {
-            size_t bl = strlen(buf);
-            if (bl + 2 >= sizeof(buf)) p_error("Path muy largo");
-            buf[bl] = PATHSEP; buf[bl+1] = '\0';
-        }
-    }
-
-    if (MKDIR(buf) != 0) { /* ignorar si existe */ }
-}
-
-// Lee imagen y clasifica en base a color predominante
-void Clasificar(const char* img_in) {
-    const char* img_out = Get_Dirhisto();
-    
-    int w, h, ch;
-
-    unsigned char* img = stbi_load(img_in, &w, &h, &ch, 0);
-
-    if (!img) 
-        p_error("Error al cargar imagen");
-
-    if (!(ch == 1 || ch == 3 || ch == 4)) 
-    {
-        stbi_image_free(img);
-        img = stbi_load(img_in, &w, &h, &ch, 3);
-        
-        if (!img) 
-            p_error("Cantidad de canales de imagen invalido");
-        
-        ch = 3;
-    }
-
-    size_t pixeles = (size_t)w * (size_t)h;
-
-    unsigned long long sumR = 0, sumV = 0, sumA = 0;
-
-    if (ch == 1) 
-    {
-        for (size_t i = 0; i < pixeles; ++i) 
-        {
-            unsigned int val = img[i];
-            sumR += val; 
-            sumV += val; 
-            sumA += val;
-        }
-    } 
-    
-    else if (ch == 3 || ch == 4) 
-    {
-        for (size_t i = 0, p = 0; i < pixeles; ++i, p += ch) 
-        {
-            sumR += img[p + 0];
-            sumV += img[p + 1];
-            sumA += img[p + 2];
-        }
-    }
-
-    const char* folder = "Rojo";
-
-    if (sumV > sumR && sumV >= sumA) 
-        folder = "Verde";
-
-    else if (sumA > sumR && sumA > sumV) 
-        folder = "Azul";
-
-    char folderOut[1024];
-    snprintf(folderOut, sizeof(folderOut), "%s%c%s", img_out, PATHSEP, folder);
-    CheckDir(img_out);
-    CheckDir(folderOut);
-
-    const char* base = GetFileN(img_in);
-
-    char out_path[1200];
-    snprintf(out_path, sizeof(out_path), "%s%c%s", folderOut, PATHSEP, base);
-    
-    int ok = stbi_write_png(out_path, w, h, ch, img, w * ch);
-    stbi_image_free(img);
-    
-    if (!ok) 
-        p_error("Error al escribir PNG.");
 }
